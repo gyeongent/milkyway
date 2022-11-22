4761c1792b031e5442393c3c1b90ee9a4f6edba5
GyeongMin Lee <59150207+gyeongent@users.noreply.github.com>
2022-11-22 15:16:56 +0900

    mobile improve

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,25 @@
 import React from "react";
+import Compass from "../compass.svg";
 
 function Footer(){
 
     const date = new Date();
 
+    function BackToTop(){
+        window.scrollTo(0,0);
+    }
+
     return(
-        <div className="footer">
-            &copy; {date.getFullYear()} Milky Way Project All rights reserved.
+        <div>
+            <div className="mobile_b2t" onClick={BackToTop}>
+                <img src={Compass} alt="Back to top" width={'30vw'}/>
+                <div>Back to top</div>
+            </div>
+            <div className="footer">
+                &copy; {date.getFullYear()} Milky Way Project All rights reserved.
+            </div>
         </div>
+        
     );
 }
 
